23f0b65c55b9beb0305415776906aa75074c99d0
tbaker60 <131214504+tbaker60@users.noreply.github.com>
2024-05-06 22:31:13 -0400

    Got puzzle to print out on a txt file. Code is setup for future work with basic functions

--- main.py
+++ main.py
@@ -13,9 +13,29 @@ def main():
 
     return(0)
 
-def generateSudoku(file=unsolvedSudoku):
+# Fills in initial values
+def generateSudoku():
+    
+    puzzle = blankPuzzle()
+    
+    # Each index represents the number used
+    # the value at each index is how many times it has been used
+    # [0] represents empty boxes
+    # For  simple or 'easy' sudoku, 3 of each number must be used and 3 numbers must be in each row, column, and box
+    usedNums = [0 for i in range(10)]
+
+    printPuzzle(puzzle, unsolvedSudoku)
+
+# Solver
+def solveSudoku(puzzle: list):
+    solvedPuzzle = blankPuzzle()
+    # Do some case based reasoning things here
+    printPuzzle(puzzle, solvedSudoku)
+
+# Initializer
+def blankPuzzle():
     # Initialize all the squares
-    topLeftCorner = array([[[0]*3]*3])
+    topLeftCorner = [[0 for i in range(3)] for j in range(3)]
     topRightCorner = topLeftCorner
     botLeftCorner = topLeftCorner
     botRightCorner = topLeftCorner
@@ -25,25 +45,24 @@ def generateSudoku(file=unsolvedSudoku):
     left = topLeftCorner
     right = topLeftCorner
     
-    #puzzle = array([[topLeftCorner, top, topRightCorner],
-    #               [left, center, right],
-    #               [botLeftCorner, bottom, botRightCorner]])
-    puzzle = array([[[[0]*3]*3], [[[0]*3]*3], [[[0]*3]*3]]*3)
+    puzzle = [[topLeftCorner, top, topRightCorner],
+                   [left, center, right],
+                   [botLeftCorner, bottom, botRightCorner]]
+    return puzzle
 
-    print(puzzle)
-    with open(file, 'w') as f:
+# Prints out a given puzzle to a given file
+def printPuzzle(puzzle: list, outputFile: str):
+    with open(outputFile, 'w') as f:
+        f.write(' -----------------------')
         for rowOfSquare in range(3):
             for innerRow in range(3):
+                f.write('\n')
                 for colOfSquare in range(3):
+                    f.write('  ')
                     for innerCol in range(3):
-                        f.write(str(puzzle[rowOfSquare][innerRow][colOfSquare][innerCol]))
-                        if innerCol == 2:
-                            f.write(' | ')
-            f.write('------------------')
-
+                        f.write(str(puzzle[rowOfSquare][innerRow][colOfSquare][innerCol])+' ')
                     
-
-
+            f.write('\n -----------------------')
 
 
 
