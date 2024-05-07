2aab55612d0bd9b5a91ebc7929ef73c422509de6
Toby Baker <131214504+tbaker60@users.noreply.github.com>
2024-05-07 19:11:50 -0400

    updated to a class based approach

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,9 @@
-# Case Based Sudoku Program
+# Constraints Based Sudoku Program
 # Final project for AI class 
 # Toby Baker and Thomas Denvir
 # 4/30/2024
 from numpy import array
+from random import randint
 
 unsolvedSudoku = "sudokuProblem.txt"
 solvedSudoku = "sudokuSolution.txt"
@@ -16,54 +17,88 @@ def main():
 # Fills in initial values
 def generateSudoku():
     
-    puzzle = blankPuzzle()
+    puzzle = Puzzle()
     
     # Each index represents the number used
     # the value at each index is how many times it has been used
     # [0] represents empty boxes
     # For  simple or 'easy' sudoku, 3 of each number must be used and 3 numbers must be in each row, column, and box
-    usedNums = [0 for i in range(10)]
+    numCoords = [[[0, 0] for i in range(9)] for j in range(10)]
+    numCts = [0 for i in range(10)]
+    for layer in range(3):
+        prevRow = [0, 0]
+        prevCol = [0, 0]
+        for numCt in range(3):
+            if numCt != 0:
+                while col != prevCol[numCt-1] and row != prevRow[numCt-1]:
+                    col = randint(0,2)
+                    row = randint(0,2)
+            else:
+                col = randint(0,2)
+                row = randint(0,2)
+            puzzle[numCt]
 
-    printPuzzle(puzzle, unsolvedSudoku)
+
+
+
+
+    puzzle.print(unsolvedSudoku)
 
 # Solver
 def solveSudoku(puzzle: list):
-    solvedPuzzle = blankPuzzle()
+    solvedPuzzle = Puzzle()
     # Do some case based reasoning things here
-    printPuzzle(puzzle, solvedSudoku)
+    puzzle.print(solvedSudoku)
 
-# Initializer
-def blankPuzzle():
-    # Initialize all the squares
-    topLeftCorner = [[0 for i in range(3)] for j in range(3)]
-    topRightCorner = topLeftCorner
-    botLeftCorner = topLeftCorner
-    botRightCorner = topLeftCorner
-    center = topLeftCorner
-    top = topLeftCorner
-    bottom = topLeftCorner
-    left = topLeftCorner
-    right = topLeftCorner
-    
-    puzzle = [[topLeftCorner, top, topRightCorner],
-                   [left, center, right],
-                   [botLeftCorner, bottom, botRightCorner]]
-    return puzzle
 
-# Prints out a given puzzle to a given file
-def printPuzzle(puzzle: list, outputFile: str):
-    with open(outputFile, 'w') as f:
-        f.write(' -----------------------')
-        for rowOfSquare in range(3):
-            for innerRow in range(3):
-                f.write('\n')
-                for colOfSquare in range(3):
-                    f.write('  ')
-                    for innerCol in range(3):
-                        f.write(str(puzzle[rowOfSquare][innerRow][colOfSquare][innerCol])+' ')
+class Puzzle:
+    # Initializes all values to 0
+    def __init__(self):
+        self.topLayer    = [Block() for i in range(3)]
+        self.middleLayer = [Block() for i in range(3)]
+        self.bottomLayer = [Block() for i in range(3)]
+        self.solved = False
+
+    # Overloads []
+    def __getitem__(self, row: int, col: int):
+        if row == 0:
+            return self.topLayer[col]
+        elif row == 1:
+            return self.middleLayer[col]
+        elif row == 2: 
+            return self.bottomLayer[col]
+        else:
+            return ("No index at location " + str(row)+', '+str(col))
+        
+    # Prints out a puzzle to a given file
+    def print(self):
+        outputFile = unsolvedSudoku
+        if self.solved:
+            outputFile = solvedSudoku
+
+        print("Printing to "+outputFile)
+        with open(outputFile, 'w') as f:
+            f.write(' -----------------------')
+            for rowOfSquare in range(3):
+                for innerRow in range(3):
+                    f.write('\n')
+                    for colOfSquare in range(3):
+                        f.write('  ')
+                        for innerCol in range(3):
+                            f.write(str(self[rowOfSquare, innerRow][colOfSquare, innerCol])+' ')
                     
             f.write('\n -----------------------')
 
+    
+
+class Block:
+    def __init__(self):
+        self = [[0 for i in range(3)] for j in range(3)]
+    
+    def __getitem__(self, row: int, col: int):
+        return self[row][col]
+    def __setitem__(self, row: int, col: int, value: int):
+        self[row][col] = value
 
 
 if __name__ == "__main__":
